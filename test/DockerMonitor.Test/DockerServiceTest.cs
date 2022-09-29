a068d4cc4a5bf391c6646a666458e2b1239a8de7
Jan-Lukas Pagel <it1-pageja@student.itech-bs14.de>
2022-09-29 15:59:33 +0200

    add unit tests

--- test/DockerMonitor.Test/DockerServiceTest.cs
+++ test/DockerMonitor.Test/DockerServiceTest.cs
@@ -1,4 +1,5 @@
 using DockerMonitor.Domain.Entities;
+using DockerMonitor.Domain.Exceptions;
 using DockerMonitor.Domain.Interfaces;
 using DockerMonitor.Services;
 using Moq;
@@ -20,5 +21,62 @@ public class DockerServiceTest
         //Assert
         result.Should().AllBeOfType<ContainerInfo>();
         result.Count().Should().Be(0);
+    }
+
+    [Fact]
+    public async void GetContainerInfo_ReturnsValidContainer()
+    {
+        //Arrange
+
+        var id = "abc";
+        CancellationToken cancellationToken = default;
+        _dockerInformationMock.Setup(info => info.GetContainer("abc", cancellationToken)).ReturnsAsync(new ContainerInfo { Id = id });
+        var dockerService = new DockerService(_dockerInformationMock.Object);
+        //Act
+        var result = await dockerService.GetContainerInfo(id);
+        //Assert
+        result.Should().BeOfType<ContainerInfo>();
+        result.Id.Should().Be(id);
+    }
+
+    [Fact]
+    public async void GetContainerInfo_ThrowContainerNotFoundException()
+    {
+        //Arrange
+
+        var id = "abc";
+        CancellationToken cancellationToken = default;
+        _dockerInformationMock.Setup(info => info.GetContainer("abc", cancellationToken)).ThrowsAsync(new ContainerNotFoundException());
+        var dockerService = new DockerService(_dockerInformationMock.Object);
+        //Act
+        var exec = () => dockerService.GetContainerInfo(id);
+        //Assert
+        await exec.Should().ThrowExactlyAsync<ContainerNotFoundException>();
+    }
+
+    [Fact]
+    public async void StartContainer_ReturnFalse()
+    {
+        // Arrange
+        CancellationToken cancellationToken = default;
+        _dockerInformationMock.Setup(info => info.StartContainer("abc", cancellationToken)).ReturnsAsync(false);
+        var dockerService = new DockerService(_dockerInformationMock.Object);
+        //Act
+        var result = await dockerService.StartContainer("abc");
+        //Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public async void StartContainer_ReturnTrue()
+    {
+        // Arrange
+        CancellationToken cancellationToken = default;
+        _dockerInformationMock.Setup(info => info.StartContainer("abc", cancellationToken)).ReturnsAsync(true);
+        var dockerService = new DockerService(_dockerInformationMock.Object);
+        //Act
+        var result = await dockerService.StartContainer("abc");
+        //Assert
+        result.Should().BeTrue();
     }
 }
